e140a252f94b9771b2d44e0883c2f395369f255d
ri2rixxx <margarita10031983@gmail.com>
2026-03-23 01:05:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/api/*": "/$1"
+}));
+server.use(router);
+
+if (process.env.NODE_ENV !== 'production') {
+  server.listen(3001, () => {
+    console.log('JSON Server is running');
+  });
+}
+
+module.exports = server;const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
 server.use(router);
 server.listen(3001, '0.0.0.0', () => {
   console.log('JSON Server is running on http://193.42.115.90:3001');
